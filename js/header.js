ad7cdea3710441e71d5f70bf5ae3a4d131451b1a
barchuk oleg <barchukoleg12@gmail.com>
2020-03-08 00:39:23 +0200

    paralax for intro, .gitignore added

---
+++ js/header.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.section-link').click(function(){
+    var id = $(this).attr('href');
+
+    $('html, body').animate({
+      scrollTop: ($(id).offset().top - 76)
+    }, 500);
+    return false;
+  });
+});
